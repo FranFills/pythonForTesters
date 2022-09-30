bd827cc8498fdbb5c48d32a51dc788ec52386012
Fisayo Obabire <frannfisayo@gmail.com>
2022-09-30 08:11:03 +0100

    task1

---
+++ tasks/task2.py
@@ -0,0 +1,14 @@
+
+# declare a raw string
+
+raw_string = r'I like to be free\nIt doesn\'t always happen\n\Hold the door.\b'
+
+# declare two strings
+string1 = 'Testify'
+string2 = ' Testar'
+
+# concatenate the strings
+role = string1 + string2
+
+# print out the concatenated string
+print(role)
