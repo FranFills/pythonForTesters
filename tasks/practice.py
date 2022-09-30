13f04aa816af5c86c9eb75a26e91c9fe670c5c8c
Fisayo Obabire <frannfisayo@gmail.com>
2022-09-30 15:38:13 +0100

    task7

---
+++ tasks/practice.py
@@ -0,0 +1,13 @@
+
+fruits = ['Apple', 'Banana', 'Cucumber']
+for fruit in fruits:
+    print('Fruit: ' + fruit)
+
+name = 'Stadium'
+for c in name:
+    print('Character: ' + c)
+
+number = 13
+while 0 < number: #< 12:
+    print('number: ', number)
+    number -= 1
