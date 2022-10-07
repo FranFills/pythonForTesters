71ce54fd1e1d4b56ec05103c5890887666c5f17e
Fisayo Obabire <frannfisayo@gmail.com>
2022-10-07 09:38:01 +0100

    task8

---
+++ tasks/task8.py
@@ -0,0 +1,14 @@
+"""
+Use for loop to iterate from 0 to 10
+For each iteration print out "Number ", i
+If the i value is equal to 2 add the continue
+If the i value is equal to 8 add the break statement
+"""
+
+nums = 10
+for num in range(nums):
+    print("Number", num)
+    if num == 2:
+        continue
+    if num == 8:
+        break
