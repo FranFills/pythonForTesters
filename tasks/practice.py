88d07f800f4d891cbef78bbf47bb484cd88aebff
Fisayo Obabire <frannfisayo@gmail.com>
2022-10-08 16:53:20 +0100

    task11

--- tasks/practice.py
+++ tasks/practice.py
@@ -24,3 +24,38 @@ hello = lambda : print('Hello World Anonymous')
 hello()
 
 accept(lambda c: print(c))
+
+def num (num1, num2=4):
+    print(num1 + num2)
+
+num(3, 9)
+
+# required parameter
+def greet (name):
+    print('Hello', name)
+
+greet('Fisayo')
+
+# default parameter
+def sumOfNumbers(firstNum =21, secondNum =12):
+    addResult = firstNum+secondNum
+    print('Result', addResult)
+
+sumOfNumbers()
+
+# keyword parameter
+def sumNumbers(firstNum, secondNum):
+    addResult = firstNum-secondNum
+    print('Result', addResult)
+
+sumNumbers(secondNum = 16, firstNum=11 )
+
+
+# arbitrary parameter
+def sNumbers(*args):
+    print('Args', args)
+
+sNumbers()
+sNumbers(0, 87, 32)
+sNumbers(7, 21, 65)
+sNumbers(2, 5, 7, 9, 'Hello')
