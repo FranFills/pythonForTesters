e1470648205a2650ab3869f511f49741f63e1833
Fisayo Obabire <frannfisayo@gmail.com>
2022-10-10 10:46:59 +0100

    task13

--- tasks/task13.py
+++ tasks/task13.py
@@ -4,3 +4,11 @@ Invoke the same function in it own body
 Invoke the function outside the function block
 """
 
+def recurse(loop):
+    print('Hello World')
+    if loop == 0:
+        return
+        #loop += 1
+    recurse(loop-1)
+
+recurse(6)
