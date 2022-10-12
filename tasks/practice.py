eb014a68bc1c46760c09b2f5a7dc3fdb4aca71fe
Fisayo Obabire <frannfisayo@gmail.com>
2022-10-12 19:40:42 +0100

    task19

--- tasks/practice.py
+++ tasks/practice.py
@@ -272,3 +272,56 @@ fisayo = Human('Fisayo')
 print(fisayo.can_walk)
 print(fisayo.leg_count)
 print(fisayo.name)
+
+class Animal:
+
+    leg_count = 4
+    group = 'Mammal'
+
+# default constructor
+    def __init__(self):
+        self.name = 'Unknown'
+
+
+class Vehicle:
+
+    can_fly = False
+    tire_count = 4
+
+# parameterized constructor
+    def __init__(self, make):
+        self.make = make
+
+    def set_tire_count(self, count):
+        self.tire_count = count
+
+    def set_flyable(self, cfly):
+        self.can_fly = cfly
+
+    def get_make_tire_count(self):
+        return self.make + ':' + str(self.tire_count)
+
+    def check_type(self):
+        if self.make == 'Aeroplane':
+            print('This is a plane')
+        else:
+            print('This is probably a car')
+
+toyota = Vehicle('Toyota')
+print('\nVehicle:', toyota.make, toyota.can_fly)
+toyota.check_type()
+
+lexus = Vehicle('Lexus')
+print('\nVehicle:', lexus.make, lexus.can_fly)
+
+
+plane = Vehicle('Aeroplane')
+plane.set_tire_count(7)
+plane.set_flyable(True)
+print('\nVehicle:', plane.make, plane.tire_count, plane.can_fly)
+print(plane.get_make_tire_count())
+
+plane.check_type()
+
+animal = Animal()
+print('\nAnimal:', animal.name, animal.group)
