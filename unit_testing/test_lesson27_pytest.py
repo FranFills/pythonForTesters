9fad29b8d9ac2112039e15863a9d2b1add57ebb0
Fisayo Obabire <frannfisayo@gmail.com>
2022-10-16 22:36:59 +0100

    unittest

--- unit_testing/test_lesson27_pytest.py
+++ unit_testing/test_lesson27_pytest.py
@@ -3,4 +3,10 @@ import lesson27
 
 def test_addition():
     assert lesson27.addition(5, 2) == 7
+    assert lesson27.addition(90, 3) == 93
+    assert lesson27.addition(30, 3) != 3
 
+def test_subtraction():
+    assert lesson27.subtraction(42, 8) == 34
+    assert lesson27.subtraction(2, 8) == -6
+    assert lesson27.subtraction(4, 8) != 4
